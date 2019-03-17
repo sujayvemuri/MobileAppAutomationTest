6b5969096b5b9ca3ce6a18f7ff1796f290f690b8
V V Sujay Kumar <vemurisujay@gmail.com>
2019-03-17 19:38:52 +0530

    Update ExampleInstrumentedTest.java
    Code added to test the Demo app

--- AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ AndroidDemo/app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -2,12 +2,30 @@ package com.mytaxi.android_demo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.RootMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import com.mytaxi.android_demo.activities.MainActivity;
+
+import org.hamcrest.Matcher;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,12 +33,76 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
+
+    /**
+     * Perform action of waiting for a specific time.
+     */
+    public static ViewAction waitFor(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Wait for " + millis + " milliseconds.";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                uiController.loopMainThreadForAtLeast(millis);
+            }
+        };
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    public void UserLogin() {
+
+        onView(withId(R.id.edt_username))
+                .perform(typeText("crazydog335"));
+        onView(withId(R.id.edt_password))
+                .perform(typeText("venture"));
+        onView(withId(R.id.btn_login))
+                .perform(click());
+    }
+
+    @Test
+    public void myTaxiTest()    {
+
+        UserLogin();
+
+        onView(isRoot())
+                .perform(waitFor(5000));
+
+        onView(withId(R.id.textSearch))
+                .perform(typeText("sa"));
+
+        onView(isRoot())
+                .perform(waitFor(5000));
+
+        onView(withText("Sarah Scott"))
+                .inRoot(RootMatchers.isPlatformPopup())
+                .perform(click());
+
+        onView(withId(R.id.fab)).perform(click());
+
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
+
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
+
     }
+
 }
